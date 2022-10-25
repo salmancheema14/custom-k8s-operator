8db7eb1401d0c84257803ca9f5f20ae372f68cf9
salmancheema14 <105018698+salmancheema14@users.noreply.github.com>
2022-10-25 15:44:12 -0700

    skeleton code for resource, controller, and finalizer

--- custom-k8s-operator/controllers/PerceptiveEngineV1Controller.cs
+++ custom-k8s-operator/controllers/PerceptiveEngineV1Controller.cs
@@ -2,9 +2,10 @@
 //     Copyright (c) Microsoft Corporation. All rights reserved.
 //  </copyright>
 
+using custom_k8s_operator.finalizers.V1;
 using custom_k8s_operator.models;
+using custom_k8s_operator.models.V1;
 using DotnetKubernetesClient;
-using IdentityModel;
 using k8s.Models;
 using KubeOps.Operator.Controller;
 using KubeOps.Operator.Controller.Results;
@@ -14,41 +15,54 @@ using Microsoft.Rest;
 
 namespace custom_k8s_operator.controllers
 {
-    [EntityRbac(typeof(MyCustomResource), Verbs = RbacVerb.All)]
-    public class MyResourceController : IResourceController<MyCustomResource>
+    [EntityRbac(typeof(PerceptiveEngineV1), Verbs = RbacVerb.All)]
+    public class PerceptiveEngineV1Controller : IResourceController<PerceptiveEngineV1>
     {
-        private readonly ILogger<MyResourceController> logger;
+        private readonly IFinalizerManager<PerceptiveEngineV1> finalizerManager;
+        private readonly ILogger<PerceptiveEngineV1Controller> logger;
         private readonly IKubernetesClient kubernetesClient;
 
-        public MyResourceController(ILogger<MyResourceController> logger, IKubernetesClient kubernetesClient)
+        public PerceptiveEngineV1Controller(
+            ILogger<PerceptiveEngineV1Controller> logger, 
+            IKubernetesClient kubernetesClient, 
+            IFinalizerManager<PerceptiveEngineV1> finalizerManager)
         {
             this.logger = logger;
             this.kubernetesClient = kubernetesClient;
+            this.finalizerManager = finalizerManager;
         }
 
-        public async Task<ResourceControllerResult> ReconcileAsync(MyCustomResource resource)
+        /// <summary>
+        /// This method is called for 'KubeOps.Operator.Kubernetes.ResourceEventType.Reconcile' events on a given entity.
+        /// https://buehler.github.io/dotnet-operator-sdk/kube-ops/KubeOps.Operator.Kubernetes.ResourceEventType.html
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        public async Task<ResourceControllerResult> ReconcileAsync(PerceptiveEngineV1 resource)
         {
             var resourceName = resource.Name();
             var resourceNamespace = resource.Namespace();
 
-            logger.LogInformation($"Reconciling {resource.Kind}:{resourceName}");
+            logger.LogInformation($"Reconciling '{resourceName}' of type '{resource.Kind}' in namespace '{resourceNamespace}'");
 
             try
             {
-                if(await this.kubernetesClient.Get<MyCustomResource>(resourceName, resourceNamespace) == null)
+                if(await this.kubernetesClient.Get<PerceptiveEngineV1>(resourceName, resourceNamespace) == null)
                 {
-                    logger.LogInformation($"{resourceName} is already deleted. No reconciliation required. Aborting operation");
+                    logger.LogInformation($"{resourceName} is already deleted. No reconciliation required. Skipping reconciliation");
                     return null;
                 }
 
                 //do something with the 'resource', e.g., get and update or instantiate an AI pipeline based on the details in the object.
+
+                //kubernetesClient.Get<V1DaemonSet>("", )
 
                 //update state
                 resource.Status.State = ResourceStates.Running;
                 await this.kubernetesClient.UpdateStatus(resource);
 
                 this.logger.LogInformation($"Updated resource status to '{resource.Status.State}'");
-                await finalizerManager.RegisterFinalizerAsync<MonitorFinalizer>(entity);
+                await finalizerManager.RegisterFinalizerAsync<PerceptiveEngineV1Finalizer>(resource);
                 return null; // This won't trigger a requeue.
             }
             catch(Exception ex)
@@ -65,14 +79,14 @@ namespace custom_k8s_operator.controllers
             }
         }
 
-        public Task StatusModifiedAsync(MyCustomResource resource)
+        public Task StatusModifiedAsync(PerceptiveEngineV1 resource)
         {
             logger.LogInformation($"{resource.Name()} called {nameof(StatusModifiedAsync)}.");
 
             return Task.CompletedTask;
         }
 
-        public async Task DeletedAsync(MyCustomResource resource)
+        public async Task DeletedAsync(PerceptiveEngineV1 resource)
         {
             logger.LogInformation($"{resource.Name()} called {nameof(DeletedAsync)}.");
         }
